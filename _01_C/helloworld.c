d6881004bea3afed78e8f1d4381c9ad82e322b4e
JimryYchao <17889982535@163.com>
2026-01-09 23:57:20 +0800

    update

--- _01_C/helloworld.c
+++ _01_C/helloworld.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-// int main(void)
-// {
-// 	printf("Hello C!");
-// }
- 
-#include <stdio.h>
-int main (void){
-printf("%ls", L"Hello C!");
-
+int main(void)
+{
+ 	printf("Hello C!");
 }
