d8b271a34f72936e843e0aa18c45a99308927e3c
JimryYchao <17889982535@163.com>
2026-01-19 23:44:22 +0800

    update

--- _01_C/helloworld.c
+++ _01_C/helloworld.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-// int main(void){
-//  	printf("Hello C!");
-// }
-
-// 文件范围
-#include <stdio.h>
-static char *Hi = "Hello C!"; // 静态存储，内部链接
-const int n = 10086;
+int main(void){
+ 	printf("Hello C!");
+}
